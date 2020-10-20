2981fd55dc0b4837c337678330577cccda88e9a7
Ronny Hsieh <ronny_hsieh@systemweb.com.tw>
2020-10-20 18:32:38 +0800

    step 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
+import { HeroesComponent } from './heroes/heroes.component'
+import { FormsModule } from '@angular/forms'
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, HeroesComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
