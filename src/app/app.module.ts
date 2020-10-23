5ca98d2ca3602d128a39f02dbf6cbe3f6aaafb9d
Ronny Hsieh <ronny_hsieh@systemweb.com.tw>
2020-10-23 14:37:40 +0800

    step 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
-
-import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
-import { HeroesComponent } from './heroes/heroes.component'
 import { FormsModule } from '@angular/forms'
 
+import { AppComponent } from './app.component'
+import { HeroesComponent } from './heroes/heroes.component'
+import { HeroDetailComponent } from './hero-detail/hero-detail.component'
+
 @NgModule({
-  declarations: [AppComponent, HeroesComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
